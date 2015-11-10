f2dc404a48f6d33111bc0cc479484e4b90533101
Boris Doesborg <bdoesborg@gmail.com>
2015-11-10 15:00:41 +0100

    start build script

--- build-platform.sh
+++ build-platform.sh
@@ -2,8 +2,11 @@
 
 echo "hello wurld"
 
-./bin/phing fetch-multisite
-./bin/phing make-multisite
-./bin/phing make-project
-./bin/phing symlink-all
-./bin/phing install-drupal
+pwd
+ls
+
+#./bin/phing fetch-multisite
+#./bin/phing make-multisite
+#./bin/phing make-project
+#./bin/phing symlink-all
+#./bin/phing install-drupal
