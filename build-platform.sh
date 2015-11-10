0d5ef5fae3f6f2aed5b2b6b37cd311c9e8623fd3
Boris Doesborg <bdoesborg@gmail.com>
2015-11-10 18:01:05 +0100

    start build script

--- build-platform.sh
+++ build-platform.sh
@@ -2,11 +2,12 @@
 
 echo "hello wurld"
 
-./bin/phing -verbose fetch-multisite
-./bin/phing -verbose make-multisite
-cp -R temp_docroot/* docroot
-cp temp_docroot/.* docroot
+#./bin/phing -verbose fetch-multisite
+#./bin/phing -verbose make-multisite
+./bin/phing -verbose make-mini-drupal
+cp -r temp_docroot/. docroot
+#cp temp_docroot/.* docroot
 rm -rf temp_docroot
 ./bin/phing -verbose symlink-all
-#./bin/phing -verbose copy-platform-settings
+./bin/phing -verbose copy-platform-settings
 #./bin/phing install-drupal
