b9615d5e7211ea4d9aaed36d7dbb032025479ac3
Boris Doesborg <bdoesborg@gmail.com>
2015-11-10 17:48:42 +0100

    start build script

--- build-platform.sh
+++ build-platform.sh
@@ -5,6 +5,7 @@ echo "hello wurld"
 ./bin/phing -verbose fetch-multisite
 ./bin/phing -verbose make-multisite
 cp -R temp_docroot/* docroot
+cp temp_docroot/.* docroot
 rm -rf temp_docroot
 ./bin/phing -verbose symlink-all
 #./bin/phing -verbose copy-platform-settings
