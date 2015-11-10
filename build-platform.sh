c12496a4a8e8faaf56daedb41f7a10cb6908da86
Boris Doesborg <bdoesborg@gmail.com>
2015-11-10 15:19:10 +0100

    start build script

--- build-platform.sh
+++ build-platform.sh
@@ -6,7 +6,7 @@ pwd
 ls
 
 ./bin/phing -verbose fetch-multisite
-#./bin/phing make-multisite
+./bin/phing -verbose make-multisite
 #./bin/phing make-project
 #./bin/phing symlink-all
 #./bin/phing install-drupal
